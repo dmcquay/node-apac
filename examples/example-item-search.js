6ff4f3297764c20ca3d3e9819921b4391777338c
Guillaume Marty <edo999@gmail.com>
2012-10-19 10:53:56 +0200

    Change coding standards

--- examples/example-item-search.js
+++ examples/example-item-search.js
@@ -2,16 +2,16 @@ var sys = require('sys'),
     OperationHelper = require('apac').OperationHelper;
 
 var opHelper = new OperationHelper({
-    awsId:     '[YOUR AWS ID HERE]',
-    awsSecret: '[YOUR AWS SECRET HERE]',
-    assocId:   '[YOUR ASSOCIATE TAG HERE]',
+  awsId: '[YOUR AWS ID HERE]',
+  awsSecret: '[YOUR AWS SECRET HERE]',
+  assocId: '[YOUR ASSOCIATE TAG HERE]'
 });
 
 opHelper.execute('ItemSearch', {
-    'SearchIndex': 'Books',
-    'Keywords': 'harry potter',
-    'ResponseGroup': 'ItemAttributes,Offers'
+  'SearchIndex': 'Books',
+  'Keywords': 'harry potter',
+  'ResponseGroup': 'ItemAttributes,Offers'
 }, function(error, results) {
-    if (error) { sys.print('Error: ' + error + "\n") }
-    sys.print("Results:\n" + sys.inspect(results) + "\n");
+  if (error) { sys.print('Error: ' + error + '\n') }
+  sys.print('Results:\n' + sys.inspect(results) + '\n');
 });
--- test/OperationHelper_test.js
+++ test/OperationHelper_test.js
@@ -1,15 +1,15 @@
-var apac = require("../lib/apac"),
-    vows = require("vows"),
-    assert = require("assert"),
-    http = require("http"),
+var apac = require('../lib/apac'),
+    vows = require('vows'),
+    assert = require('assert'),
+    http = require('http'),
     event = require('events');
 
 // the basic setup
 var OperationHelper = apac.OperationHelper;
 var opHelper = new OperationHelper({
-        awsId:     'test',
-        awsSecret: 'test+test',
-        assocId:   'test-01',
+  awsId: 'test',
+  awsSecret: 'test+test',
+  assocId: 'test-01'
 });
 
 // some hand-rolled mocks
@@ -20,38 +20,38 @@ var request_emitter = new(event.EventEmitter),
 request_emitter.end = function() { return true };
 
 response_emitter.setEncoding = function(something) {return true};
-response_emitter.statusCode = 200
+response_emitter.statusCode = 200;
 
 http.createClient = function(port, host) {
-    return {
-      request: function(it, doesnt, matter) {
-          return request_emitter;
-      }
-    };
-}
+  return {
+    request: function(it, doesnt, matter) {
+      return request_emitter;
+    }
+  };
+};
 
 // now for the tests!
 vows.describe('OperationHelper execute').addBatch({
-    'results': {
-        topic: function() {
-            opHelper.execute('ItemSearch', {
-                    'SearchIndex': 'Books',
-                    'Keywords': 'harry potter',
-                    'ResponseGroup': 'ItemAttributes,Offers'
-            }, this.callback);
+  'results': {
+    topic: function() {
+      opHelper.execute('ItemSearch', {
+        'SearchIndex': 'Books',
+        'Keywords': 'harry potter',
+        'ResponseGroup': 'ItemAttributes,Offers'
+      }, this.callback);
 
-            // use the emitter to emit the appropriate events
-            request_emitter.emit('response', response_emitter);
-            response_emitter.emit('data', '<it><is><some>xml</some></is></it>');
-            response_emitter.emit('end');
-        },
+      // use the emitter to emit the appropriate events
+      request_emitter.emit('response', response_emitter);
+      response_emitter.emit('data', '<it><is><some>xml</some></is></it>');
+      response_emitter.emit('end');
+    },
 
-        'are json': function(error, result) {
-            assert.isObject(result);
-        },
+    'are json': function(error, result) {
+      assert.isObject(result);
+    },
 
-        'have the right structure': function(error, result) {
-            assert.deepEqual(result, {is: {some: 'xml'}});
-        }
+    'have the right structure': function(error, result) {
+      assert.deepEqual(result, {is: {some: 'xml'}});
     }
+  }
 }).run();
