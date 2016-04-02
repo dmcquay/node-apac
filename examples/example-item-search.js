78f58463ecb2919b4bf2228550b97c9b892a8015
Dustin McQuay <dmcquay@gmail.com>
2016-04-01 22:36:52 -0600

    Add Promise interface to OperationHelper.execute

--- examples/example-item-search.js
+++ examples/example-item-search.js
@@ -1,16 +1,16 @@
 var util = require('util'),
-    OperationHelper = require('../lib/apac').OperationHelper;
+    OperationHelper = require('../lib/apac').OperationHelper
 
 var opHelper = new OperationHelper({
   awsId: '[YOUR AWS ID HERE]',
   awsSecret: '[YOUR AWS SECRET HERE]',
   assocId: '[YOUR ASSOCIATE TAG HERE]'
-});
+})
 
 opHelper.execute('ItemSearch', {
   'SearchIndex': 'Books',
   'Keywords': 'harry potter',
   'ResponseGroup': 'ItemAttributes,Offers'
 }, function(err, results) {
-	console.log(results);
-});
+	console.log(results)
+})
